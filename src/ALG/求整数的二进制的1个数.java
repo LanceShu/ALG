7aebcd6673725d9676ee3d71b047edafb2095f9e
LanceShu <869924650@qq.com>
2017-12-20 00:04:23 +0800

    update code

--- src/ALG/求整数的二进制的1个数.java
+++ src/ALG/求整数的二进制的1个数.java
@@ -3,7 +3,8 @@ package ALG;
 public class 求整数的二进制的1个数 {
 
     public static void main(String[] args) {
-        System.out.println(numberOfOne(15));
+        System.out.println(8<<1);
+        System.out.println(numberOfOne2(15));
     }
 
     public static int numberOfOne(int n ){
@@ -12,6 +13,17 @@ public class 求整数的二进制的1个数 {
         for(int i = 0;i<32;i++){
             count += (n&1);
             n >>= 1;
+        }
+
+        return count;
+    }
+
+    public static int numberOfOne2(int n ){
+        int count = 0;
+
+        while(n != 0){
+            count++;
+            n = (n-1) & n;
         }
 
         return count;
