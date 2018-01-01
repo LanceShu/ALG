4709d1714c2a3119727483009d98b552a0bf4c58
LanceShu <869924650@qq.com>
2018-01-01 23:59:52 +0800

    update ALG

--- src/ALG/堆排序.java
+++ src/ALG/堆排序.java
@@ -5,9 +5,56 @@ public class 堆排序 {
     public static int[] arrays = {50,10,90,30,70,40,80,60,20};
 
     public static void main(String[] args) {
-        HeapSort();
+//        HeapSort();
+        printArray(arrays);
         for(int a : arrays){
             System.out.print(a + " ");
+        }
+    }
+
+
+    public static void heapSort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+
+        buildMaxHeap(array);
+
+        for (int i = array.length - 1; i >= 1; i--) {
+            ArrayUtils.exchangeElements(array, 0, i);
+
+            maxHeap(array, i, 0);
+        }
+    }
+
+    private static void buildMaxHeap(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+
+        int half = array.length / 2;
+        for (int i = half; i >= 0; i--) {
+            maxHeap(array, array.length, i);
+        }
+    }
+
+    private static void maxHeap(int[] array, int heapSize, int index) {
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+
+        int largest = index;
+        if (left < heapSize && array[left] > array[index]) {
+            largest = left;
+        }
+
+        if (right < heapSize && array[right] > array[largest]) {
+            largest = right;
+        }
+
+        if (index != largest) {
+            ArrayUtils.exchangeElements(array, index, largest);
+
+            maxHeap(array, heapSize, largest);
         }
     }
 
